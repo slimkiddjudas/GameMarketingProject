b3ecabe6d564d7c08a91a2c7a44eda2a9314fa65
Mert Ömer BAKAR <95905493+mertomer@users.noreply.github.com>
2022-11-24 18:41:19 +0300

    Update UserData.js

--- src/Data/UserData.js
+++ src/Data/UserData.js
@@ -1,6 +1,6 @@
 const userData = [
     {id:1, userName:"umutcerk", password:"12345"},
-    {id:2, userName:"bizontr", password:"3131"},
+    {id:2, userName:"bizontr", password:"3123"},
     {id:3, userName:"stepcek", password:"karasevda55"}
 ]
 
